8eefe7a0dbccfd923f49a49af4fe744b37c12adc
triagustinaputri <42725273+triagustinaputri@users.noreply.github.com>
2018-10-29 19:43:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,39 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title> Data Siswa </title>
 </head>
 <body>
-    <form action="class.php" method="post">
-        Masukan angka 1<input type="text" name="input1"><br>
-        Masukan angka 2<input type="text" name="input2"><br>
-        Pilih Operasi
-        <select name="operasi">
-            <option value="+">+</option>
-            <option value="-">-</option>
-            <option value="*">*</option>
-            <option value="/">/</option>
-        </select>
-        <br>
-        <input type="submit" value="Kirim">
-    </form>
+    <a href="tambah.php"><input type="button" value="Tambah Data"></a>
+                <table>
+                    <thead>
+                        <tr>
+                            <th> Nama </th>
+                            <th> Nim </th>
+                            <th> Tgl_lahir </th>
+                            <th> Aksi </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            require("data.php");
+                            $result = $data -> view();
+                            $isi = mysqli_num_rows($result);
+
+                            if($isi != 0) {
+                                while($row = mysqli_fetch_assoc($result)) {
+                                    ?>
+                                    <tr>
+                                        <td><?php echo $row['nama']; ?></td>
+                                        <td><?php echo $row['nim']; ?></td>
+                                        <td><?php echo $row['tgl_lahir']; ?></td>
+                                        <td><a href="edit.php?nim=<?php echo $row['nim']; ?>"><input type="button" value="Edit"></a> <a href="data.php?hapus=<?php echo $row['nim']; ?>"><input type="button" value="Hapus"></a></td>
+                                    </tr>
+                                    <?php
+                                }
+                            }
+                        ?>
+                    </tbody>
+                </table>
 </body>
 </html>
